977777da72765b3d4ea74398deb755726e74922d
MtSanti <matheus.santiago@sptech.school>
2023-06-04 22:33:49 -0300

    arquivos

---
+++ oficial/site/public/js/funcoes.js
@@ -0,0 +1,61 @@
+// sessão
+function validarSessao() {
+    // aguardar();
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    var email_do_usuario = document.getElementById("email_do_usuario");
+    var nome_do_usuario = document.getElementById("nome_do_usuario");
+    
+
+    var b_usuario = document.getElementById("b_usuario");
+
+    if (email != null && nome != null) {
+        window.alert(`Seja bem-vindo, ${nome}!`);
+        // b_usuario.innerHTML = nome;
+        email_do_usuario.innerHTML = email
+        nome_do_usuario.innerHTML = nome
+        // finalizarAguardar();
+    } else {
+        window.location = "../login.html";
+        
+    }
+}
+
+function limparSessao() {
+    // aguardar();
+    sessionStorage.clear();
+    // finalizarAguardar();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+// function aguardar() {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "flex";
+// }
+
+// function finalizarAguardar(texto) {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "none";
+
+//     var divErrosLogin = document.getElementById("div_erros_login");
+//     if (texto) {
+//         divErrosLogin.style.display = "flex";
+//         divErrosLogin.innerHTML = texto;
+//     }
+// }
+
+
+// modal
+function mostrarModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "flex";
+}
+
+function fecharModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "none";
+}
+
---
+++ oficial/site/src/database/script-tabelas.sql
@@ -0,0 +1,10 @@
+create database PlayOffs;
+use PlayOffs;
+
+create table Usuario (
+Nome varchar(45),
+Email varchar(45), 
+senha varchar(45)
+);
+
+select * from Usuario;
